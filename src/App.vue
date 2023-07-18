3a1f01a0835c83d4519b6db9935197c8581652a0
raqib907 <106022396+raqib907@users.noreply.github.com>
2023-07-18 23:25:58 +0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+       Landingpage
+      </nav>
+    </header>
+    <main>
+      <div> hello</div>
+    </main>
+    <footer>
+      <<h1>This is footer</h1>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+:root {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+
+</style>
